46700c098bbfdf16cd75ebe4329595aa5604abf2
Joe Guinness <guinness@Guinness-MacBook-Pro.local>
2017-11-14 18:50:42 -0500

    updated source files

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,33 @@
 
 using namespace Rcpp;
 
+// getLinvEntries
+NumericMatrix getLinvEntries(NumericVector covparms, StringVector covfun_name, NumericMatrix locs, IntegerMatrix NNarray);
+RcppExport SEXP _aldodevel_getLinvEntries(SEXP covparmsSEXP, SEXP covfun_nameSEXP, SEXP locsSEXP, SEXP NNarraySEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< NumericVector >::type covparms(covparmsSEXP);
+    Rcpp::traits::input_parameter< StringVector >::type covfun_name(covfun_nameSEXP);
+    Rcpp::traits::input_parameter< NumericMatrix >::type locs(locsSEXP);
+    Rcpp::traits::input_parameter< IntegerMatrix >::type NNarray(NNarraySEXP);
+    rcpp_result_gen = Rcpp::wrap(getLinvEntries(covparms, covfun_name, locs, NNarray));
+    return rcpp_result_gen;
+END_RCPP
+}
+// LinvMultFromEntries
+NumericVector LinvMultFromEntries(NumericMatrix LinvEntries, NumericVector z, IntegerMatrix NNarray);
+RcppExport SEXP _aldodevel_LinvMultFromEntries(SEXP LinvEntriesSEXP, SEXP zSEXP, SEXP NNarraySEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< NumericMatrix >::type LinvEntries(LinvEntriesSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type z(zSEXP);
+    Rcpp::traits::input_parameter< IntegerMatrix >::type NNarray(NNarraySEXP);
+    rcpp_result_gen = Rcpp::wrap(LinvMultFromEntries(LinvEntries, z, NNarray));
+    return rcpp_result_gen;
+END_RCPP
+}
 // MaternFun
 Rcpp::NumericMatrix MaternFun(Rcpp::NumericMatrix& distmat, Rcpp::NumericVector covparms);
 RcppExport SEXP _aldodevel_MaternFun(SEXP distmatSEXP, SEXP covparmsSEXP) {
@@ -48,6 +75,8 @@ END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
+    {"_aldodevel_getLinvEntries", (DL_FUNC) &_aldodevel_getLinvEntries, 4},
+    {"_aldodevel_LinvMultFromEntries", (DL_FUNC) &_aldodevel_LinvMultFromEntries, 3},
     {"_aldodevel_MaternFun", (DL_FUNC) &_aldodevel_MaternFun, 2},
     {"_aldodevel_OrderedCompLik", (DL_FUNC) &_aldodevel_OrderedCompLik, 5},
     {"_aldodevel_OrderedGroupCompLik", (DL_FUNC) &_aldodevel_OrderedGroupCompLik, 4},
